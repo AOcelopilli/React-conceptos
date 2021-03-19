534c8a98222f2ca876e152bcc6cae94227f82203
angelp495 <angeljpa95@gmail.com>
2021-03-18 20:30:12 -0600

    comunicacion entre componentes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Estado from "./components/Estado";
 import RenderizadoCondicional from "./components/RenderizadoCondicional";
 import RenderizadoElementos from "./components/RenderizadoElementos";
 import { EventosES6, EventosES7, MasSobreEventos } from "./components/Eventos";
+import Padre from "./components/ComunicacionComponentes";
 
 function App() {
   let nombre = "Angel";
@@ -66,6 +67,8 @@ function App() {
           <EventosES7 />
           <hr />
           <MasSobreEventos />
+          <hr />
+          <Padre />
         </section>
       </header>
     </div>
